8991ed8a7fc79a873b3baaa1a521eb3522fdc5e5
Ömer Bahrioğlu <bahriogluomer@gmail.com>
2024-12-12 04:06:39 +0300

    fixed icons import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [
+		sveltekit(),
+		tailwindcss(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
